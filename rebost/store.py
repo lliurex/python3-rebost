a4043e2fbaf6128dc336544ec07cf79fca0621ba
lliurex <juanma1980@gmail.com>
2025-09-29 11:47:42 +0200

    Clean code. Add categories sorted by installs

--- rebost/store.py
+++ rebost/store.py
@@ -15,7 +15,6 @@ class client():
 		self.dbg=True
 		logging.basicConfig(format='%(message)s')
 		self.user=''
-		self.n4dkey=''
 		if kwargs:
 			self.user=kwargs.get('user','')
 		if self.user=='':
@@ -62,77 +61,7 @@ class client():
 		self.rebost=dbus.Interface(rebost,"net.lliurex.rebost")
 	#def _connect
 	
-	def execute(self,action,args='',extraParms=''):
-		self._testConnection()
-		procId=0
-		if isinstance(args,str):
-			args=[args]
-		if args:
-			for arg in args:
-				if ":" in arg:
-					package=arg.split(":")
-					extraParms=package[-1]
-					package=package.pop(0)
-				else:
-					package=arg
-				try:
-					if action=='install':
-						procId=self.installApp(package,extraParms)
-					elif action=='remove':
-						procId=self.removeApp(package,extraParms)
-					elif action=='test':
-						procId=self.testInstall(package,extraParms)
-					elif action=='remote_install':
-						procId=self.remoteInstall(package,extraParms)
-					elif action=='search':
-						procId=self.searchApp(package)
-					elif action=='getCategories':
-						procId=self.getCategories()
-					elif action=='getFreedesktopCategories':
-						procId=self.getFreedesktopCategories()
-					elif action=='list':
-						procId=self.getAppsInCategory(package,extraParms)
-					elif action=='show':
-						procId=self.showApp(package)
-					elif action=='match':
-						procId=self.matchApp(package)
-					elif action=='enableGui':
-						procId=self.enableGui(arg)
-				except dbus.exceptions.DBusException as e:
-					desc=e.get_dbus_name()
-					if desc!="org.freedesktop.DBus.Error.AccessDenied":
-						print("Dbus Error: %s"%e)
-					procId=0
-				except Exception as e:
-					procId=0
-					print("Err: %s"%e)
-			#	finally:
-			#		self.rebost=None
-		else:
-			try:
-				if action=='update':
-					procId=self.rebost.update()
-				#if action=='fullupdate':
-				#	procId=self.rebost.fullUpdate()
-				elif action=='load':
-					procId=self.rebost.load(package,extraParms)
-				elif action=='export':
-					procId=self.rebost.export()
-			except dbus.exceptions.DBusException as e:
-				procId=0
-				print("Dbus Error: %s"%e)
-			except Exception as e:
-				procId=0
-				print("Err: %s"%e)
-			finally:
-				self.rebost=None
-		if isinstance(procId,str)==False:
-			procId=str(procId)
-		return(procId)
-
 	def _testConnection(self):
-		if self.n4dkey=='':
-			self.n4dkey=self._getN4dKey()
 		self._connect()
 	#def _testConnection
 
@@ -163,7 +92,7 @@ class client():
 	def getCategories(self):
 		self._testConnection()
 		categories=self.rebost.getCategories()
-		return(str(categories))
+		return(categories)
 	#def getCategories(self):
 
 	def getAppsPerCategory(self):
@@ -181,8 +110,22 @@ class client():
 	def getAppsInstalled(self):
 		self._testConnection()
 		installedApps=self.rebost.getAppsInstalled()
-		return(str(installedApps))
+		return(installedApps)
 	#def getInstalledApps
+
+	def getAppsInstalledPerCategory(self):
+		apps=self.getAppsInstalled()
+		installedApps={}
+		categories=self.rebost.getFreedesktopCategories()
+		for cat in categories:
+			installedApps[cat]=[]
+		apps=json.loads(self.rebost.getAppsInstalled())
+		for app in apps:
+			for cat in app["categories"]:
+				if cat in installedApps.keys():
+					installedApps[cat].append(app)
+		return(installedApps)
+	#def getAppsInstalledPerCategory
 
 	def getUpgradableApps(self,user=''):
 		self._testConnection()
@@ -208,15 +151,6 @@ class client():
 		self._testConnection()
 		apps=self.rebost.searchAppByUrl(url)
 		return(apps)
-	#def searchApp
-
-	def matchApp(self,package):
-		self._testConnection()
-		try:
-			package=self.rebost.match(package,self.user)
-		except Exception as e:
-			print(e)
-		return(str(package))
 	#def searchApp
 
 	def showApp(self,package):
@@ -257,31 +191,6 @@ class client():
 		return("")
 	#def searchApp
 
-	def getAppStatus(self,package,bundle):
-		self._testConnection()
-		package=self.testInstall(package,bundle,self.user)
-		try:
-			package=json.loads(package)[0]
-		except:
-			package={}
-		epiFile=package.get('script','')
-		status="1"
-		if os.path.isfile(epiFile):
-			status=self.getEpiPkgStatus(epiFile)
-			#os.remove(epiFile)
-		return(status)
-
-	def installApp(self,package,bundle):
-		self._testConnection()
-		if bundle=="zomando":
-			zmdPath=os.path.join("/usr/share/zero-center/zmds","{}.zmd".format(package))
-			if os.path.isfile(zmdPath)==True:
-				installResult=subprocess.run([zmdPath]).returncode
-		else:
-			installResult=self.rebost.install(package,bundle,self.user,self.n4dkey)
-		return(str(installResult))
-	#def installApp
-
 	def getExternalInstaller(self):
 		self._testConnection()
 		installer=""
@@ -292,115 +201,14 @@ class client():
 		return(installer)
 	#def installPpp
 
-	def removeApp(self,package,bundle):
-		self._testConnection()
-		if bundle=="zomando":
-			bundle="package"
-		removeResult=self.rebost.remove(package,bundle,self.user,self.n4dkey)
-		return(str(removeResult))
-	#def removeApp
-
-	def commitInstall(self,package,bundle,state):
-		self._testConnection()
-		commitResult=self.rebost.commitInstall(package,bundle,state)
-		return(str(commitResult))
-	#def commitInstall
-
-	def getEpiPkgStatus(self,epiScript):
-		self._testConnection()
-		pkgStatus=self.rebost.getEpiPkgStatus(epiScript)
-		return(str(pkgStatus))
-	#def getEpiPkgStatus
-
-	def updatePkgData(self,pkgname,rebostPkg):
-		self.rebost.updatePkgData(pkgname,json.dumps(rebostPkg))
-
-	def remoteInstall(self,package,bundle,user=''):
+	def remoteInstall(self,package,bundle,user=''): #PENDING (remote-installer)
+		return()
 		self._testConnection()
 		if user=='':
 			user=self.user
 		remoteResult=self.rebost.remote_install(package,bundle,user)
 		return(str(remoteResult))
 	#def remoteInstall
-
-	def enableGui(self,enable):
-		self._testConnection()
-		enable=False
-		if isinstance(enable,str):
-			if enable.lower()!="false":
-				enable=True
-		elif isinstance(enable,int)==True:
-			enable=bool(enable)
-		#enabled=self.rebost.enableGui(enable)
-		enabled="true"
-		return(str(enabled))
-	#def enableGui(self,enable):
-
-	def forceUpdate(self,procId=0):
-		self._connect()
-		return({})
-		bus=self.rebost.fullUpdate()
-		progressDict=json.loads(bus)
-		self.rebost=None
-		return(progressDict)
-	#def forceUpdate
-
-	def update(self,force=False):
-		self._testConnection()
-		updateResult={}
-		try:
-			updateResult=self.rebost.update(force)
-		except Exception as e:
-			time.sleep(10)
-			print(e)
-		return(str(updateResult))
-	#def update
-
-	def restart(self):
-		self._testConnection()
-		restartResult=self.rebost.restart()
-		return(str(restartResult))
-	#def restart
-
-	def getProgress(self,procId=0):
-		self._connect()
-		bus=self.rebost.getResults()
-		progressDict=json.loads(bus)
-		self.rebost=None
-		return(progressDict)
-	#def getProgress
-	
-	def getResults(self,procId=0):
-		self._connect()
-		bus=self.rebost.getResults()
-		results=json.loads(bus)
-		self.rebost=None
-		return(results)
-	#def getResults
-
-	def disableFilters(self):
-		self._connect()
-		result=self.rebost.disableFilters()
-		return(result)
-	#def disableFilters
-	
-	def getFiltersEnabled(self):
-		self._connect()
-		result=self.rebost.getFiltersEnabled()
-		return(result)
-	#def getFiltersEnabled
-	
-	def getPlugins(self):
-		pass
-
-	def commitData(self):
-		self.rebost.commitData()
-	#def commitData
-
-	def getLockStatus(self):
-		self._connect()
-		result=self.rebost.getLockStatus()
-		return(result)
 
 	def lock(self):
 		self._connect()
@@ -414,12 +222,3 @@ class client():
 		return(result)
 	#def unlock
 	
-	def _getN4dKey(self):
-		n4dkey=''
-		try:
-			with open('/etc/n4d/key') as file_data:
-				n4dkey = file_data.readlines()[0].strip()
-		except:
-			pass
-		return(n4dkey)
-	#def _getN4dKey
